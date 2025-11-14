6e6c3551adb139880513c2f1b3db0bcc61ae74cd
Anass <12345678+yourusername@users.noreply.github.com>
2025-11-15 00:15:03 +0100

    Done

---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahamouda <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/16 09:29:11 by ahamouda          #+#    #+#             */
+/*   Updated: 2025/08/16 14:47:36 by ahamouda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_swap(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i <= size - 1)
+	{
+		j = i + 1;
+		while (j <= size - 1)
+		{
+			if (tab[j] < tab[i])
+				ft_swap(&tab[i], &tab[j]);
+			j++;
+		}
+		i++;
+	}
+}
