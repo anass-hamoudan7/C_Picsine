c664851de0c1b32c3dbf98322042946289dc63fa
Anass <12345678+yourusername@users.noreply.github.com>
2025-11-15 00:21:31 +0100

    Done

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -12,17 +12,17 @@
 
 #include <unistd.h>
 
-void ft_putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
 	if (nb == -2147483648)
 	{
 		write(1, "-2147483648", 11);
-		return;
+		return ;
 	}
 	if (nb < 0)
 	{
